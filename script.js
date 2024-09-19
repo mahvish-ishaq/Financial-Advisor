d01a3d83dfd7411a7d515d1c745d1c0ed1f0fb56
Mahvish Ishaq <138211115+mahvish-ishaq@users.noreply.github.com>
2024-09-19 21:27:59 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// Add event listener to the Get Started button
+document.querySelector('button').addEventListener('click', function() {
+    // Toggle the visibility of the services section
+    document.querySelector('#services').classList.toggle('visible');
+});
